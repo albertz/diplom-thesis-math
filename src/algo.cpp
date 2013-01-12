81a7fdbeb4edce6f4f908f3319142166f003ce15
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-01-12 21:15:21 +0100

    code

--- src/algo.cpp
+++ src/algo.cpp
@@ -2,6 +2,11 @@
 typedef int Int;
 //typedef ... M2; // Matrix 2x2
 
+// In many cases, we wont use this variable and hardcode
+// it for degree 2. However, we define it here,
+// so we can point it out in some cases.
+static const int HermDegree = 2;
+
 struct M2T {
 	// [[a,b],[\bar b, c]]
 	Int a, b, c;
@@ -20,7 +25,13 @@ struct PrecisionF {
 	
 };
 
+
 struct ReductionMatrices_Calc {
-	CurlS_Generator
+	
+	
+	CurlS_Generator curlS;
+	
+	
+	
 };
 
