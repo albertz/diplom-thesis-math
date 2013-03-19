bc0c61129ccf595d100db0ed800bd6790ecb4ae6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-03-19 18:53:54 +0100

    algo.py

--- src/algo.py
+++ src/algo.py
@@ -4,6 +4,8 @@
 # Init curlS = {}.
 
 # Step 3. Iterate S \in Mat_2^T(Z). Add to curlS. iterate by denominator.
+# S_11 and S_22 (diagonal entries) are positive.
+# S can be changed arbitrarily by GL(2, \ZZ).
 
 # Step 3a. Choose B>0 as limit for precision curlF.
 # TODO: how? dependent on S?
@@ -15,3 +17,5 @@
 #   This comes last. It's written in Dern.
 
 # If check fails, goto step 3 and enlarge curlS.
+
+# Otherwise, reconstruct fourier expansion.
