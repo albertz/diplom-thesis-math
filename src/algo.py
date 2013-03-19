07130a62c085efe16fc02b846779a2527325a45a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-03-19 18:30:03 +0100

    algo.py

---
+++ src/algo.py
@@ -0,0 +1,17 @@
+
+# Step 1. Iterate through square-free numbers l, starting at 1.
+
+# Init curlS = {}.
+
+# Step 3. Iterate S \in Mat_2^T(Z). Add to curlS. iterate by denominator.
+
+# Step 3a. Choose B>0 as limit for precision curlF.
+# TODO: how? dependent on S?
+
+# Step 4. Calculate restriction matrix.
+
+# Step 5. dimension check
+# TODO
+#   This comes last. It's written in Dern.
+
+# If check fails, goto step 3 and enlarge curlS.
