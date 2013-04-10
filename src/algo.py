3aefb6a777ac6c39bdffdf5566bc418089c86575
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-10 13:02:46 +0200

    algo

--- src/algo.py
+++ src/algo.py
@@ -1,28 +1,4 @@
 
-# Step 1. Iterate through square-free numbers l, starting at 1.
-
-# Init curlS = {}.
-
-# Step 3. Iterate S \in Mat_2^T(Z). Add to curlS. iterate by denominator.
-# S_11 and S_22 (diagonal entries) are positive.
-# S positive definite?
-# S can be changed arbitrarily by GL(2, \ZZ).
-
-# Step 3a. Choose B>0 as limit for precision curlF.
-# TODO: how? dependent on S?
-
-# Step 4. Calculate restriction matrix.
-# This is ReductionMatrices_Calc.calcMatrix().
-
-# Step 5. dimension check
-# TODO
-#   This comes last. It's written in Dern.
-
-# If check fails, goto step 3 and enlarge curlS.
-
-# Otherwise, reconstruct fourier expansion.
-
-
 import algo_cython as C
 
 def reloadC():
@@ -45,7 +21,7 @@ def reloadC():
 	assert(libdl.dlclose(so._handle) == 0)
 	reload(C)
 	
-def test_algo():
+def test_algo_calcMatrix():
 	calc = C.Calc()
 	calc.init(D = -4, HermWeight = 10)
 
@@ -54,3 +30,36 @@ def test_algo():
 	
 	calc.calcMatrix()
 	return calc.getMatrix()
+
+def modform(D, HermWeight):
+	"Main algo"
+	
+	calc = C.Calc()
+	calc.init(D = D, HermWeight = HermHeight)
+
+	# Step 1. Iterate through square-free numbers l, starting at 1.
+	# Init curlS = {}.
+
+	# Step 3. Iterate S \in Mat_2^T(Z). Add to curlS. iterate by denominator.
+	# S_11 and S_22 (diagonal entries) are positive.
+	# S positive definite.
+	# S can be changed arbitrarily by GL(2, \ZZ).
+
+	while True:
+		S = calc.getNextS()
+
+		# Step 3a. Choose B>0 as limit for precision curlF.
+		# TODO: how? dependent on S?
+		# ... calc.setPrecisionLimit(B)
+
+		# Step 4. Calculate restriction matrix.
+		M = calc.calcMatrix()
+
+		# Step 5. dimension check
+		
+		# TODO
+		#   This comes last. It's written in Dern.
+
+		# If check fails, goto step 3 and enlarge curlS.
+
+		# Otherwise, reconstruct fourier expansion.
