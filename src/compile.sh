53ef1b7634d0619e2fed7e9457f086d7a11640f1
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-10 09:13:28 +0200

    compile.sh fixed. works now

--- src/compile.sh
+++ src/compile.sh
@@ -4,11 +4,35 @@ SageDir="/Applications/sage-5.4"
 SageLocalIncludeDir="$SageDir/local/include"
 SageIncludeDir="$SageDir/devel/sage/c_lib/include"
 SageExtIncludeDir="$SageDir/devel/sage/sage/ext"
+PythonIncludeDir="$SageLocalIncludeDir/python2.7"
 
 cython \
+	-I $PythonIncludeDir \
 	-I $SageIncludeDir \
 	-I $SageLocalIncludeDir \
 	-I $SageExtIncludeDir \
 	--cplus \
-	algo.pyx
+	algo_cython.pyx
 
+c++ \
+	-std=gnu++11 -stdlib=libc++ \
+	-I $PythonIncludeDir \
+	-I $SageIncludeDir \
+	-I $SageLocalIncludeDir \
+	-I $SageExtIncludeDir \
+	-c \
+	algo_cython.cpp
+
+# on linux:
+#["ld"] +
+#["-L/usr/local/lib"] +
+#infiles +
+#options +
+#["-lc"] +
+#["-shared", "-o", outfile]
+
+# mac:
+libtool -dynamic \
+	algo_cython.o \
+	-undefined dynamic_lookup \
+	-o algo_cython.so
