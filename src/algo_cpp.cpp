141a140d4af981655a9a6dad8a9813a5a53650d3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-17 02:05:11 +0200

    fix

--- src/algo_cpp.cpp
+++ src/algo_cpp.cpp
@@ -151,7 +151,7 @@ int calcPrecisionDimension(const PrecisionF& F, ElemOfS S) {
 	DOMAIN_CHECK(S.det(F.D) > 0);
 	DOMAIN_CHECK(F.B > 0);
 	// Note: might be zero
-	return (F.B * S.a + F.B * S.c - S.absBMupper(F.D, F.B));
+	return (F.B * S.a + F.B * S.c - S.absBMupper(F.D, 2 * F.B));
 }
 
 typedef M2T_Odual ElemOfF;
