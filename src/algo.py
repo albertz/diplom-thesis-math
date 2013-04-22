18ef3f3403dec3fdcc96059513021cc535bd1805
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-22 13:42:00 -0400

    more. doesnt work yet

--- src/algo.py
+++ src/algo.py
@@ -106,8 +106,7 @@ def modform(D, HermWeight, B_cF=10):
 		if Verbose: print "done"
 		restriction_fe_expansions = ell_modform_fe_expansions_l.intersection( M_S_module )
 		herm_modform_fe_expannsion_S = M_S.solve_right( restriction_fe_expansions.basis_matrix().transpose() )
-		herm_modform_fe_expannsion_S += M_S.right_kernel()
-		# TODO: or row_space?
+		#herm_modform_fe_expannsion_S = M_S.right_kernel().basis_matrix() * herm_modform_fe_expannsion_S
 		herm_modform_fe_expannsion_S_module = herm_modform_fe_expannsion_S.column_module()
 
 		herm_modform_fe_expannsion = herm_modform_fe_expannsion.intersection( herm_modform_fe_expannsion_S_module )
