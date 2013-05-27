999f180a018eccdcd4b7c5a76a497ed9cd901db3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-05-27 14:20:22 +0200

    copyright, license

--- src/reduceGL.hpp
+++ src/reduceGL.hpp
@@ -4,6 +4,27 @@
 
 // https://github.com/martinra/psage/blob/paper_computing_jacobi_forms/psage/modform/hermitianmodularforms/hermitianmodularformd2_fourierexpansion_cython.pyx
 
+//===============================================================================
+//
+// Copyright (C) 2009 Dominic Gehre, Martin Raum
+// Translated to C++ by Albert Zeyer (2013).
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+//===============================================================================
+
+
 #include <math.h>
 #include <iostream>
 #include <stdlib.h>
