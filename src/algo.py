7f2539ab10db94b77ed6fbf3c7d8fa80ff6c38cd
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-05-31 13:40:13 +0200

    cleanup

--- src/algo.py
+++ src/algo.py
@@ -195,7 +195,6 @@ def modform_restriction_info(calc, S, l):
 	HermWeight = calc.HermWeight
 	B_cF = calc.B_cF
 	reducedCurlFSize = calc.matrixColumnCount
-	herm_modform_fe_expannsion = FreeModule(QQ, reducedCurlFSize)
 
 	# Step 4. Calculate restriction matrix. Via calc.calcMatrix() (algo_cpp.cpp).
 	# Note that calcMatrix() depends on the current internal calc.curlS set
